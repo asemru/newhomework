e67d57a704ef28fbbf3e0910472e51aa0e828c19
asemru <95555506+asemru@users.noreply.github.com>
2024-10-31 21:57:59 +1200

    Add files via upload

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -19,13 +19,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
-
+from task4.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='menu.html')),
     path('books/', TemplateView.as_view(template_name='books.html')),
-    path('games/', TemplateView.as_view(template_name='videogames.html')),
+    #path('games/', TemplateView.as_view(template_name='videogames.html')),
+    path('games/', index, name='games'),
     path('ok/', TemplateView.as_view(template_name='of couse.html')),
     path('opana/', TemplateView.as_view(template_name='opana.html')),
     path('magas/', TemplateView.as_view(template_name='magazin.html'))
